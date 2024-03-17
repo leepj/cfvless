0d0eb5b2b0ad294dac9298427e607b37f871ee26
lpjdragon <lpjdragon@hotmail.com>
2024-03-17 17:06:06 +0800

    Update _worker1.js

--- _worker1.js
+++ _worker1.js
@@ -1,1 +1,11 @@
-
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = '140.238.10.85'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
